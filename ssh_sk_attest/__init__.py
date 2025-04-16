27f4486db6e8d8aff7519405a5c0afb9c6364991
Carl Tashian <carl@smallstep.com>
2025-04-16 09:30:41 -0500

    Remove python

--- ssh_sk_attest/__init__.py
+++
@@ -1,3 +0,0 @@
-from .attestation import parse_attestation, verify_attestation, parse_pubkey, AttestationResult
-
-__all__ = ['parse_attestation', 'verify_attestation', 'parse_pubkey', 'AttestationResult'] 
